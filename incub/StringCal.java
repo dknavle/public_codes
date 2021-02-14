c78ad481ef19280eb1a36649592825d1b670b405
Dnyaneshwar Navle <dknavle5@gmail.com>
2021-02-14 18:40:10 +0530

    Allow the Add method to handle an unknown amount of numbers

--- incub/StringCal.java
+++ incub/StringCal.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 public class StringCal {
     public static void main(String[] args) {
         
         StringCal stringCal = new StringCal();
         int ans;
+        // input from user
+        Scanner sc = new Scanner(System.in);
+        String calString = sc.next();
 
-        ans = stringCal.Add("");
+        ans = stringCal.Add(calString);
 
         System.out.println(ans);
     }
 
     // 
-    int Add(String ins){
+    int Add(String calString){
+        String[] arr = calString.split(",");
+        int fans = 0;
 
-        return 0;
+        for(String str : arr){
+            try{
+                fans += Integer.parseInt(str);
+            }catch(Exception e){
+                System.out.println(" Exception Occurred : "+e.getMessage());
+                return 0;
+            }
+        }
+        return fans;
     }
 }
