944da209e7f9021bb48e404ec40288f71f9dbe6e
Dnyaneshwar Navle <dknavle5@gmail.com>
2021-02-14 18:25:58 +0530

    Add method added.

---
+++ incub/StringCal.java
@@ -0,0 +1,17 @@
+public class StringCal {
+    public static void main(String[] args) {
+        
+        StringCal stringCal = new StringCal();
+        int ans;
+
+        ans = stringCal.Add("");
+
+        System.out.println(ans);
+    }
+
+    // 
+    int Add(String ins){
+
+        return 0;
+    }
+}
